d4fe43082b3298d8ff0d8496a4aae5ea7567d5de
Alex Malyuk <alex.malyuk@gmail.com>
2018-10-17 15:06:10 +0300

    Log4net added

---
+++ MD.Log/SimpleLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.Log
+{
+    public class SimpleLog
+    {
+        private static object sync = new object();
+        public static void Write(Exception ex)
+        {
+            //try
+            //{
+            // Путь .\\Log
+            string pathToLog = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
+            if (!Directory.Exists(pathToLog))
+                Directory.CreateDirectory(pathToLog); // Создаем директорию, если нужно
+            string filename = Path.Combine(pathToLog, "Simple.log");
+            string fullText = string.Format("[{0:dd.MM.yyy HH:mm:ss.fff}] [{1}.{2}()] {3}\r\n", DateTime.Now, ex.TargetSite?.DeclaringType, ex.TargetSite?.Name, ex.Message);
+            lock (sync)
+            {
+                File.AppendAllText(filename, fullText, Encoding.GetEncoding("Windows-1251"));
+                }
+            //}
+            //catch
+            //{
+            //    // Перехватываем все и ничего не делаем
+            //}
+        }
+    }
+}
